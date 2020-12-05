8be70982d1e202b0525b2aab9a5b1df181e10772
Yaroslav <y.surilov@infomir.com>
2020-12-05 21:13:33 +0200

    add json tools to libs, complete Toggl summary response parser

--- tools/report.sh
+++ tools/report.sh
@@ -8,6 +8,8 @@
 # TOGGL_WORKSPACE_ID - see https://github.com/toggl/toggl_api_docs/blob/master/chapters/workspaces.md
 # ------------------------------
 
+. ../libs/json.sh
+
 TOGGL_API_URL='https://api.track.toggl.com/reports/api/v2/summary'
 YESTERDAY="$(date --date='yesterday' +%F)"
 TODAY="$(date +%F)"
@@ -15,7 +17,10 @@ TODAY="$(date +%F)"
 url="$TOGGL_API_URL?workspace_id=$TOGGL_WORKSPACE_ID&since=$YESTERDAY&until=$TODAY&user_agent=api_test"
 
 if hash curl; then
-        curl -v -u "$TOGGL_API_TOKEN:api_token" "$url"
+        response=$(curl -s -u "$TOGGL_API_TOKEN:api_token" "$url")
+        data=$(echo "$response" | json_parse 'data[].items[].title.time_entry')
+
+        #echo "$data"
     elif hash wget; then
         wget --user="$TOGGL_API_TOKEN" --password='api_token' "$url"
     else
