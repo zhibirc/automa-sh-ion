753d9ac3e0be559e20f36506a3ca23cd27cff7b2
Yaroslav <y.surilov@infomir.com>
2020-12-23 09:55:07 +0200

    add trace for debug, version and help, refactor

--- tools/light.brute.sh
+++ tools/light.brute.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # ------------------------------
 # Lightweight brute-force for access to not-so-important inner services.
 # Oftentimes it's much faster than asking and useless waiting for dumb password for required service.
@@ -11,22 +11,31 @@
 # shellcheck disable=SC1090
 . "$HOME/$CLI_TOOLS_PATH/libs/colors.sh"
 
+VERSION='0.0.1'
+HELP='Usage: CMD http://username:@example.com/ [http://:password@domain.com/ [http://domain.com/]]'
+
 if [[ $# -eq 0 ]]; then
     echo -e "${COLOR_RED}Invalid call syntax${COLOR_RESET}"
-    echo 'Usage: CMD http://username:@example.com/ [http://:password@domain.com/ [http://domain.com/]]'
+    echo "$HELP"
 
     exit 1
 fi
 
-targets=("$@")
+declare -r targets=("$@")
+declare -a targets_map
+
+# prepare targets
+# note that there are no strict checks here
+for target in "${targets[@]}"; do
+    # if we have some credentials
+    if [[ "$target" == *"@"* ]]; then
+        url=
+        #targets_map["$target"]=
+    fi
+done
+
 
 function generate () {
-    echo pass
-
-    return 0
-}
-
-function prepare () {
     echo pass
 
     return 0
