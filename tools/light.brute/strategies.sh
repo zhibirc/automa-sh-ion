e428fe54622f22053ffc1f2982eb84618013ff2e
Yaroslav <y.surilov@infomir.com>
2020-12-29 15:40:10 +0200

    fix output handling for port scanning, other refactor

--- tools/light.brute/strategies.sh
+++ tools/light.brute/strategies.sh
@@ -9,11 +9,13 @@
 # @zhibirc
 # ------------------------------
 
-declare MIN_LENGTH=1
-declare -a PORT_LIST=(21 22 23 25 53 80 110 139 162 389 443 445 512 513 514 993 1433 1521 3306 3389 5432 5900 5901 8000 8080 6667)
+declare -i MIN_RANDOM_LENGTH=1
+declare -a PORT_SCAN_LIST=(21 22 23 25 53 80 110 139 162 389 443 445 512 513 514 993 1433 1521 3306 3389 5432 5900 5901 8000 8080 6667)
+#declare -i PORT_SCAN_THREADS=4
+
 
 function generate_random () {
-    length=${1:=$MIN_LENGTH}
+    length=${1:=$MIN_RANDOM_LENGTH}
 
     tr -dc a-z0-9 < /dev/urandom | head -c "$length" | xargs
 }
@@ -21,7 +23,7 @@ function generate_random () {
 
 function scan () {
     target=$1
-    ports=${2:=${PORT_LIST[@]}}
+    #threads=${2:=$PORT_SCAN_THREADS}
 
-    nmap -T4 -P0 --open "$target" -p "${ports[@]}" -oX ./logs/scan.txt
+    nmap -T4 -A -v -Pn --open "$target" -p "${PORT_SCAN_LIST[@]}" -oN ./logs/scan.txt
 }
