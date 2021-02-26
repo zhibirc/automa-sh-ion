ac39cda24a0994b33903a9703f8e7a529ea75005
Yaroslav <y.surilov@infomir.com>
2021-02-26 19:08:52 +0200

    add debug options, file header, handle including libs to scaler

--- tools/scaler.sh
+++ tools/scaler.sh
@@ -1,24 +1,47 @@
 #!/bin/bash -e
 # ------------------------------
 # Resize/scale of image/audio/video/text files.
-# Author: me :)
+# Written by Yaroslav Surilov aka zhibirc <zhibirc.echo@gmail.com> on 2021-03
 # ------------------------------
 
 # for debug
-set -o xtrace
+set -o xtrace -o nounset -o pipefail
 
-# load required libraries
-# shellcheck disable=SC1090
-. "$(pwd)/tools/libs/colors.sh"
+readonly BASE_DIR="$(dirname "$0")"
 
-# firstly parse given options
+if [[ -f "$BASE_DIR/libs/styles.sh" ]]; then
+    # shellcheck disable=SC1090
+    . "$BASE_DIR/libs/styles.sh"
+elif [[ -d tools && -d tools/libs ]]; then
+    cd tools || { echo >&2 "Tools directory exists but I can't cd there."; exit 1; }
 
-if [[ -z "$*" ]]; then
-    echo 'No options found!'
-	exit 1
+    . "./libs/styles.sh"
+else
+    echo 'Please cd into the tools before running this script.'; exit 1;
 fi
 
-while getopts 'iavt' $option; do
+
+help () {
+    echo -e "${RED}$0${STYLE_RESET}"
+
+    cat <<EOF
+Usage examples:
+
+-h, --help
+Print this Help and exit.
+-v, --version
+Print software version and exit.
+-d, --debug
+Enable debugging mode with a lot of execution details.
+
+EOF
+}
+
+if [[ -z "$*" ]]; then
+    help 'Too few arguments\n' && exit 1
+fi
+
+while getopts 'hiavt' $option; do
     case "$option" in
         i) echo 'images';;
         a) echo 'audio';;
