414d221781bdca984af5bc105b8316fce5bf82ad
Yaroslav Surilov <ysurilov@lohika.com>
2021-05-13 11:47:59 +0300

    add creator for React components boilerplate

---
+++ tools/react.component.creator.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+unsetopt MULTIOS
+
+readonly COMPONENTS_DIR='components'
+readonly CURRENT_DIR="$(basename "$(pwd)")"
+readonly COMPONENT_NAME="$1"
+
+[[ "$CURRENT_DIR" != "$COMPONENTS_DIR" ]] && printf %s\\n "Please cd into the \"$COMPONENTS_DIR\" before running this script" && exit 1
+[[ -z "$COMPONENT_NAME" ]] && printf %s\\n 'Component name to create is absent, specify it as an argument' && exit 1
+[[ -d ./"$COMPONENT_NAME" ]] && printf %s\\n "Component $COMPONENT_NAME is probably exists, check name correctness" && exit 1
+
+mkdir "$COMPONENT_NAME"
+cd "$COMPONENT_NAME" || exit 1
+
+# index.js and its content
+touch index.js
+printf %s\\n "export * from './$COMPONENT_NAME';" > ./index.js
+
+# <Component>.js and its content
+file_name="$COMPONENT_NAME.js"
+touch "$file_name"
+cat > ./"$file_name" <<EOF
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import styles from './$COMPONENT_NAME.module.scss'
+
+export function $COMPONENT_NAME ( props ) {}
+
+$COMPONENT_NAME.propTypes = {};
+EOF
+
+# associated SCSS module and its content
+file_name="$COMPONENT_NAME.module.scss"
+touch "$file_name"
+cat > ./"$file_name" <<EOF
+@import '../../styles/variables';
+
+.body {}
+EOF
+
+exit 0
